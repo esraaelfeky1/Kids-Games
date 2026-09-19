b15ae254a539610404458c60686199dd9eeab76e
esraa1lfkyl <esraaelfeky75@gmail.com>
2026-09-19 13:59:49 +0300

    Updates

--- assets/index-Ckeak6aI.js
+++ assets/index-Ckeak6aI.js
@@ -8233,7 +8233,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     .main-board-responsive { 
       width: 78vw !important; 
       max-width: 330px !important;
-      bottom: -194% !important; 
+      bottom: -194px !important; 
     }
     
     /* رفع شخصية الولد والبقرة للأعلى بحوالي 20 بكسل إضافية */
