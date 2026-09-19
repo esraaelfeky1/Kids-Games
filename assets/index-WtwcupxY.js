4b09db4ca338e140e14090b88c630a0bf46a264f
esraa1lfkyl <esraaelfeky75@gmail.com>
2026-09-19 14:15:50 +0300

    Updates

--- assets/index-WtwcupxY.js
+++ assets/index-WtwcupxY.js
@@ -8242,7 +8242,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
     .main-board-responsive { 
       width: 78vw !important; 
       max-width: 330px !important;
-      bottom: 58% !important; 
+      bottom: 78% !important; 
     }
     
     /* رفع شخصية الولد والبقرة للأعلى لتتناسب مع المربع الجديد */
